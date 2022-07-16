0d60afee814fc2e3ce2bb786b6fc9c99e38e978c
Ricardo Magalhães <ricardospilotes@gmail.com>
2022-07-16 04:50:44 -0300

    Add files via upload

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -12,8 +12,237 @@ ScrollReveal().reveal(".saudacao", {
   duration: 2000,
 });
 
-ScrollReveal().reveal(".weather", {
-  origin: "top",
-  distance: "5rem",
+ScrollReveal().reveal(".top3", {
+  duration: 3000,
+});
+
+ScrollReveal().reveal(".ana", {
+  duration: 3000,
+});
+
+ScrollReveal().reveal(".ozzy1", {
+  duration: 3000,
+});
+
+ScrollReveal().reveal(".mon", {
+  duration: 3000,
+});
+
+ScrollReveal().reveal(".a_mais", {
+  origin: "left",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".mais_ouvida", {
+  origin: "left",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".s-artista1", {
+  origin: "left",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".s-musica", {
+  origin: "left",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".spotify", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".s-spotify", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".h-headbanger", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".img_headbanger", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".s-playlist", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".rollingstone", {
+  origin: "left",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".tmdqa", {
+  origin: "left",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".ultimo_disco", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".last_disc", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".s-artista", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".s-disc_name", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".mangalivre", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".s-mangalivre", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".yes_mangas", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".s-yes_mangas", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".berserk", {
+  origin: "left",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".claymore", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".snk", {
+  origin: "left",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".kny", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".cdz", {
+  origin: "left",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".rurouni_kenshin", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".continue", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".superanimes", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".s-superanimes", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".watching1", {
+  origin: "left",
+  distance: "7rem",
+  duration: 3000,
+});
+
+ScrollReveal().reveal(".watching2", {
+  origin: "left",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".proximo", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".netflix", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".s-netflix", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".stremio", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".s-stremio", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".topflix", {
+  duration: 2000,
+});
+ScrollReveal().reveal(".s-topflix", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".c-filme", {
+  origin: "right",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".continue2", {
+  origin: "left",
+  distance: "7rem",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".serie1", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".serie2", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".serie3", {
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".serie4", {
   duration: 2000,
 });
