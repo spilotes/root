1e85ff1b81c36e7d7bba67067e99ac081fd1d58f
Ricardo Magalhães <ricardospilotes@gmail.com>
2022-07-16 05:41:40 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,47 @@
+function musicas() {
+  window.scrollTo({
+    top: 600,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function mangas() {
+  window.scrollTo({
+    top: 2100,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function animes() {
+  window.scrollTo({
+    top: 3455,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function movies() {
+  window.scrollTo({
+    top: 4165,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function channels() {
+  window.scrollTo({
+    top: 5160,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function football() {
+  window.scrollTo({
+    top: 5757,
+    left: 0,
+    behavior: "smooth",
+  });
+}
